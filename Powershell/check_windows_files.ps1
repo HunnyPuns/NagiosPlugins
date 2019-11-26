1aec0170075f893ce48975955949351a71e2e604
HunnyPuns <8877103+HunnyPuns@users.noreply.github.com>
2019-11-26 16:48:16 -0600

    Further work on check_windows_files.ps1

--- Powershell/check_windows_files.ps1
+++ Powershell/check_windows_files.ps1
@@ -1,23 +1,28 @@
 ﻿param (
     [Parameter(Mandatory=$true)][string]$checkPath,
-    [bool]$checkFileExists = $true,
-    [string]$checkFileName = "",
-    [int]$checkFileCount = 0
+
+    [Parameter(Mandatory=$false,ParameterSetName='exists')][switch]$exists,
+    [Parameter(Mandatory=$false,ParameterSetName='exists')][bool]$shouldexist,
+
+    [Parameter(Mandatory=$false,ParameterSetName='size')][switch]$size,
+    [Parameter(Mandatory=$false,ParameterSetName='size')][string]$sizewarning,
+    [Parameter(Mandatory=$false,ParameterSetName='size')][string]$sizecritical,
+
+    [Parameter(Mandatory=$false,ParameterSetName='number')][switch]$number,
+    [Parameter(Mandatory=$false,ParameterSetName='number')][int]$numwarning,
+    [Parameter(Mandatory=$false,ParameterSetName='number')][int]$numcritical
+
 )
 
 #Setting global error action preference
 #Will probably revisit this when I add the verbose switch
 $ErrorActionPreference = "SilentlyContinue"
-
-function parseSwitches {
-    #TODO: This script will have several ways of monitoring files.
-    #Right now it only does 1 thing. But as we add more, I'm going to change
-    #how the command line switches work.
-}
+[int]$exitcode = 2
+[decimal]$version = 0.5
 
 function sanitizePath {
     #TODO: Need to figure out how to sanitize a path in Powershell.
-    #I want people to be able to just do C:\Path\To\File, not C:\\Path\\To\\File
+    #I want people to be able to do C:\Path\To\File, not C:\\Path\\To\\File
     param (
         [Parameter(Mandatory=$true)][string]$checkPath
     )
@@ -35,7 +40,7 @@ function checkFileExists {
     $returnBool = $false
         
     if (Get-CimInstance -ClassName CIM_LogicalFile `
-        -Filter "Name='$path'" `
+        -Filter "Name='$Path'" `
         -KeyOnly `
         -Namespace root\cimv2) {
 
@@ -52,16 +57,71 @@ function checkFilesInDirectory {
     )
     $returnInt = 0
 
+    $returnInt = ((Get-ChildItem $Path) | Where-Object Mode -NotLike 'd*').Count
+    
     return $returnInt
 }
 
+function checkFileSize {
+    param (
+        [Parameter(Mandatory=$true)][string]$Path
+    )
+    $returnInt = 0
+
+    $returnint = (Get-CimInstance -ClassName CIM_LogicalFile `
+                    -Filter "Name='$Path'" `
+                    -KeyOnly `
+                    -Namespace root\cimv2).FileSize
+
+    return $returnInt
+}
+
+<#
+function processOutput {
+    param (
+        [Parameter(Mandatory=$true)][string]$
+    )
+}
+#>
+
 ## MAIN SCRIPT ##
 
-#Check if a specific file or directory exists
-if (checkFileExists -Path $checkPath) {
-    echo "I found the file $checkPath!"
+if ($exists -eq $true) {
+    #Check if a specific file or directory exists
+    if (checkFileExists -Path $checkPath) {
+        echo "I found the file $checkPath!"
+        $exitcode = 0
+    }
+    else {
+        echo "I did not find the file, $checkPath"
+        $exitcode = 2
+    }
 }
-else {
-    echo "I did not find the file, $checkPath"
+elseif ($size -eq $true) {
+    #Check the size of a specific file
+    echo (checkFileSize -Path $checkPath)
+
+    #Need to go through processOutput to return Nagios data
+    
+}
+elseif ($number -eq $true) {
+    #Check the number of files in a directory
+    
+    if ((Get-CimInstance -ClassName CIM_LogicalFile `
+            -Filter "Name=$checkPath'" `
+            -KeyOnly `
+            -Namespace root\cimv2).FileType -ne "File Folder") {
+
+        echo "The path specified is not a directory."
+
+    }
+    else {
+        $myvar = (checkFilesInDirectory -Path $checkPath)
+        echo "The number of files in $checkPath is: $myvar"    
+    }
 }
 
+
+
+
+exit $exitcode
