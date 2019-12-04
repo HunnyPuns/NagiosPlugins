0086ddb8ac1136ddbbabf22c9156c76a0af1e1f3
HunnyPuns <8877103+HunnyPuns@users.noreply.github.com>
2019-12-04 16:00:42 -0600

    Initial commit of Powershell\check_windows_files.ps1

--- Powershell/check_windows_files.ps1
+++ Powershell/check_windows_files.ps1
@@ -5,8 +5,8 @@
     [Parameter(Mandatory=$false,ParameterSetName='exists')][bool]$shouldexist,
 
     [Parameter(Mandatory=$false,ParameterSetName='size')][switch]$size,
-    [Parameter(Mandatory=$false,ParameterSetName='size')][string]$sizewarning,
-    [Parameter(Mandatory=$false,ParameterSetName='size')][string]$sizecritical,
+    [Parameter(Mandatory=$false,ParameterSetName='size')][int]$sizewarning,
+    [Parameter(Mandatory=$false,ParameterSetName='size')][int]$sizecritical,
 
     [Parameter(Mandatory=$false,ParameterSetName='number')][switch]$number,
     [Parameter(Mandatory=$false,ParameterSetName='number')][int]$numwarning,
@@ -17,7 +17,8 @@
 #Setting global error action preference
 #Will probably revisit this when I add the verbose switch
 $ErrorActionPreference = "SilentlyContinue"
-[int]$exitcode = 2
+[int]$exitCode = 2
+[string]$exitMessage = "CRITICAL: something wicked happened"
 [decimal]$version = 0.5
 
 function sanitizePath {
@@ -55,7 +56,7 @@ function checkFilesInDirectory {
     param (
         [Parameter(Mandatory=$true)][string]$Path
     )
-    $returnInt = 0
+    [int]$returnInt = 0
 
     $returnInt = ((Get-ChildItem $Path) | Where-Object Mode -NotLike 'd*').Count
     
@@ -76,52 +77,95 @@ function checkFileSize {
     return $returnInt
 }
 
-<#
-function processOutput {
+function processCheck {
     param (
-        [Parameter(Mandatory=$true)][string]$
+        [Parameter(Mandatory=$true)][int]$checkResult,
+        [Parameter(Mandatory=$true)][int]$warningThresh,
+        [Parameter(Mandatory=$true)][int]$criticalThresh,
+        [Parameter(Mandatory=$false)][string]$returnMessage
     )
+
+    [array]$returnArray
+
+    if ($checkResult -gt $criticalThresh) {
+        
+        $returnArray = @(2, "CRITICAL: $returnMessage")
+    }
+    elseif ($checkResult -le $criticalThresh -and $checkResult -gt $warningThresh) {
+        
+        $returnArray = @(1, "WARNING: $returnMessage")
+    }
+    else {
+        
+        $returnArray = @(0, "OK: $returnMessage")
+    }
+
+    return $returnArray
+
 }
-#>
+
 
 ## MAIN SCRIPT ##
 
 if ($exists -eq $true) {
     #Check if a specific file or directory exists
     if (checkFileExists -Path $checkPath) {
-        echo "I found the file $checkPath!"
-        $exitcode = 0
+        $exitMessage = "OK: I found the file $checkPath!"
+        $exitCode = 0
     }
     else {
-        echo "I did not find the file, $checkPath"
-        $exitcode = 2
+        $exitMessage = "CRITICAL: I did not find the file, $checkPath"
+        $exitCode = 2
     }
 }
 elseif ($size -eq $true) {
     #Check the size of a specific file
-    echo (checkFileSize -Path $checkPath)
+    #Write-Output (checkFileSize -Path $checkPath)
 
-    #Need to go through processOutput to return Nagios data
+    $cimObj = Get-CimInstance -ClassName CIM_LogicalFile `
+            -Filter "Name='$checkPath'" `
+            -KeyOnly `
+            -Namespace root\cimv2 
+    
+    if ($cimObj.FileType -eq "File Folder") {
+        $exitMessage = "UNKNOWN: $checkPath, is a directory. Directories are not currently supported"
+        $exitCode = 3
+    }
+    else {
+        $processArray = processCheck -checkResult $cimObj.FileSize `
+                     -warningThresh $sizeWarning `
+                     -criticalThresh $sizeCritical `
+                     -returnMessage "File size is $($cimObj.FileSize)"
+        
+        #Come back to this and find out why an array with 2 elements isn't starting from 0
+        $exitCode = $processArray[1]
+        $exitMessage = $processArray[2]
+    }
     
 }
 elseif ($number -eq $true) {
     #Check the number of files in a directory
     
     if ((Get-CimInstance -ClassName CIM_LogicalFile `
-            -Filter "Name=$checkPath'" `
+            -Filter "Name='$checkPath'" `
             -KeyOnly `
             -Namespace root\cimv2).FileType -ne "File Folder") {
 
-        echo "The path specified is not a directory."
+        Write-Outpute "The path specified is not a directory."
 
     }
     else {
-        $myvar = (checkFilesInDirectory -Path $checkPath)
-        echo "The number of files in $checkPath is: $myvar"    
+        $numFiles = (checkFilesInDirectory -Path $checkPath)
+        $processArray = processCheck -checkResult $numFiles `
+                        -warningThresh $numwarning `
+                        -criticalThresh $numcritical `
+                        -returnMessage "Number of files is $numFiles"
+
+        $exitCode = $processArray[1]
+        $exitMessage = $processArray[2]
     }
 }
 
 
-
-
-exit $exitcode
+Write-Output $exitMessage
+exit [int]$exitCode
