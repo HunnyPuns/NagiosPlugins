9a211b3a2b1629e41000e5943a23bf9eb9c93569
HunnyPuns <8877103+HunnyPuns@users.noreply.github.com>
2022-05-16 21:57:13 -0500

    Changed line endings to CRLF, 'cause, y'know, Windows. Also added help! get-help check_windows_files.ps1

--- Powershell/check_windows_files.ps1
+++ Powershell/check_windows_files.ps1
@@ -1,4 +1,42 @@
-﻿param (
+<#
+.DESCRIPTION
+This is a Powershell based plugin for Nagios and Nagios-Like systems. This plugin can check and report on the following:
+* Whether or not a file exists (does not support directories just yet)
+* Checks the size of a specified file
+* Checks the number of files in a specified directory
+Remember, thresholds must be breached before they are thrown.
+E.g. numwarning 10 will need the number of files to be 11 or higher to throw a WARNING.
+.SYNOPSIS
+Nagios plugin to check whether a file exists (does not support directories just yet), the size of a specified file, or the number of files in a specified directory.
+.NOTES
+This plugin currently does not have a helper function to work with directories. Currently you need to have two backslashes in your directory paths. E.g. C:\\Monitoring\\MyDir\\
+.PARAMETER checkPath
+The path to the file or directory you wish to monitor. This is needed for all types of checks performed by the plugin.
+.PARAMETER exists
+The file specified should exist. If it does not, throw a CRITICAL.
+.PARAMETER shouldnotexist
+The file specified should not exist. If it does, throw a CRITICAL.
+.PARAMETER size
+Telling the plugin you wish to monitor the size of a specified file.
+.PARAMETER sizewarning
+Telling the plugin the size (in bytes) that the file can be before it throws a WARNING.
+.PARAMETER sizecritical
+Telling the plugin the size (in bytes) that the file can be before it throws a CRITICAL.
+.PARAMETER number
+Telling the plugin you wish to monitor the number of files in a specified directory.
+.PARAMETER numwarning
+Telling the plugin the number of files that can be in the directory before it throws a WARNING.
+.PARAMETER numcritical
+Telling the plugin the number of files that can be in the directory before it throws a CRITICAL.
+.EXAMPLE
+PS> .\check_windows_files.ps1 -checkPath C:\\Monitoring\\MyDir\\somelogfile.log -exists
+.EXAMPLE
+PS> .\check_windows_files.ps1 -checkPath C:\\Monitoring\\MyDir\\somelogfile.log -size -sizewarning 4000000000 -sizecritical 8000000000
+.EXAMPLE
+PS> .\check_windows_files.ps1 -checkPath C:\\Monitoring\\MyDir\\ -number -numwarning 4 -numcritical 8
+#>
+
+param (
     [Parameter(Mandatory=$true)][string]$checkPath,
     #[Parameter(Mandatory=$false)][switch]$verbose,
 
@@ -40,7 +78,7 @@ function checkFileExists {
     )
 
     $returnBool = $false
-        
+
     if (Get-CimInstance -ClassName CIM_LogicalFile `
         -Filter "Name='$Path'" `
         -KeyOnly `
@@ -60,7 +98,7 @@ function checkFilesInDirectory {
     [int]$returnInt = 0
 
     $returnInt = ((Get-ChildItem $Path) | Where-Object Mode -NotLike 'd*').Count
-    
+
     return $returnInt
 }
 
@@ -89,15 +127,15 @@ function processCheck {
     [array]$returnArray
 
     if ($checkResult -gt $criticalThresh) {
-        
+
         $returnArray = @(2, "CRITICAL: $returnMessage")
     }
     elseif ($checkResult -le $criticalThresh -and $checkResult -gt $warningThresh) {
-        
+
         $returnArray = @(1, "WARNING: $returnMessage")
     }
     else {
-        
+
         $returnArray = @(0, "OK: $returnMessage")
     }
 
@@ -107,7 +145,6 @@ function processCheck {
 
 
 ## MAIN SCRIPT ##
-
 if ($exists -eq $true) {
     #Check if a specific file or directory exists
     #exists does not get run through the processCheck function as the variables
@@ -139,8 +176,8 @@ elseif ($size -eq $true) {
     $cimObj = Get-CimInstance -ClassName CIM_LogicalFile `
             -Filter "Name='$checkPath'" `
             -KeyOnly `
-            -Namespace root\cimv2 
-    
+            -Namespace root\cimv2
+
     if ($cimObj.FileType -eq "File Folder") {
         $exitMessage = "UNKNOWN: $checkPath, is a directory. Directories are not currently supported"
         $exitCode = 3
@@ -150,16 +187,16 @@ elseif ($size -eq $true) {
                      -warningThresh $sizeWarning `
                      -criticalThresh $sizeCritical `
                      -returnMessage "File size is $($cimObj.FileSize) | 'File Size'=$($cimObj.FileSize);$sizeWarning;$sizeCritical"
-        
+
         #Come back to this and find out why an array with 2 elements isn't starting from 0
         $exitCode = $processArray[1]
         $exitMessage = $processArray[2]
     }
-    
+
 }
 elseif ($number -eq $true) {
     #Check the number of files in a directory
-    
+
     if ((Get-CimInstance -ClassName CIM_LogicalFile `
             -Filter "Name='$checkPath'" `
             -KeyOnly `
@@ -174,7 +211,7 @@ elseif ($number -eq $true) {
                         -warningThresh $numwarning `
                         -criticalThresh $numcritical `
                         -returnMessage "Number of files is $numFiles | 'Number of Files'=$numFiles;$numwarning;$numcritical"
-        
+
         #Come back to this and find out why an array with 2 elements isn't starting from 0
         $exitCode = $processArray[1]
         $exitMessage = $processArray[2]
